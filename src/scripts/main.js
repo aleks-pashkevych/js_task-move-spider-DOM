d841d51bc0a3c4d6b79a7e4b468f447531fbb6a6
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-01-10 08:53:10 +0200

    looks working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,17 @@
 
 document.addEventListener('click', (e) => {
   // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+  const wallEl = wall.getBoundingClientRect();
+  const spiderEl = spider.getBoundingClientRect();
+
+  let x = e.pageX - wallEl.left - wall.clientLeft - spiderEl.width / 2;
+  let y = e.pageY - wallEl.top - wall.clientTop - spiderEl.height / 2;
+
+  x = Math.max(0, Math.min(x, wall.clientWidth - spider.clientWidth));
+  y = Math.max(0, Math.min(y, wall.clientHeight - spider.clientHeight));
+
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
 });
